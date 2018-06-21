3f4d9f6dd90fcf1303ee34da66fad87243d1fd06
«VitaliyDanevych» <vitaliy.danevych@life.com.ua>
2018-06-21 15:35:11 +0300

    Udated parcing SITE_ID LTE sites

--- 4g_cell_status.cmd
+++ 4g_cell_status.cmd
@@ -11,6 +11,7 @@ rm ${wdir}/4g_cell_status.txt
 /opt/ericsson/bin/amosbatch -p 30 -t 3 ${wdir}/list_erbs.txt "lt all;s+;prox;l+ ${wdir}/4g_cell_status.txt;st cell;lget . eNBid;l-;q" ${wdir}/log
 #/opt/ericsson/bin/amosbatch -p 30 -t 3 ${wdir}/list_erbs.txt "lt all;s+;prox;l+ ${wdir}/4g_cell_status.txt;st cell;l-;q" ${wdir}/log
 
+
 # Status 4G sites are located below
 rm ${wdir}/4g_cell_status_parsed.txt
 echo 'AdmState;Op.State;MO' > ${wdir}/4g_cell_status_parsed.txt
@@ -18,10 +19,20 @@ cat ${wdir}/4g_cell_status.txt | grep EUtranCellFDD | egrep -v 'password|uservar
 perl -pi -e 's/SHUTTINGDOWN/LOCKED/g' ${wdir}/4g_cell_status_parsed.txt
 cp -p ${wdir}/4g_cell_status_parsed.txt ${wdir}/data/4g_cell_status_parsed_$(date '+DATE: %d_%m_%Y_%H_%M' | sed 's/DATE: //').txt
 
-# Site_ID 4G sites are located below
+
+# Site_ID 4G sites -- old version
+##rm ${wdir}/4g_cell_status_parsed_enodeb_id.txt
+##echo 'SITE;SITE_ID' > ${wdir}/4g_cell_status_parsed_enodeb_id.txt
+##cat ${wdir}/4g_cell_status.txt | grep -v ERBS_ | egrep '1 ENodeBFunction|lget . eNBid' | gsed -r -e 's/^(RN_.+?)>.*$/\1 /g' | gsed -r 's/^.+?ENodeBFunction.+?eNBId\s+(\w+).*$/\1 /g' | gsed -e 's/\n/;/g' | perl -pi -e 's/\n/\;/g;' | perl -pi -e 's/\;RN_/\nRN_/g;' | sed 's/ //g' | cut -c -16 >> ${wdir}/4g_cell_status_parsed_enodeb_id.txt
+##cp -p ${wdir}/4g_cell_status_parsed_enodeb_id.txt ${wdir}/data/4g_cell_status_parsed_enodeb_id_$(date '+DATE: %d_%m_%Y_%H_%M' | sed 's/DATE: //').txt
+
+
+# Site_ID 4G sites -- new version
 rm ${wdir}/4g_cell_status_parsed_enodeb_id.txt
+rm ${wdir}/4g_cell_status_parsed_enodeb_id_temp.txt
 echo 'SITE;SITE_ID' > ${wdir}/4g_cell_status_parsed_enodeb_id.txt
-cat ${wdir}/4g_cell_status.txt | grep -v ERBS_ | egrep '1 ENodeBFunction|lget . eNBid' | gsed -r -e 's/^(RN_.+?)>.*$/\1 /g' | gsed -r 's/^.+?ENodeBFunction.+?eNBId\s+(\w+).*$/\1 /g' | gsed -e 's/\n/;/g' | perl -pi -e 's/\n/\;/g;' | perl -pi -e 's/\;RN_/\nRN_/g;' | sed 's/ //g' | cut -c -16 >> ${wdir}/4g_cell_status_parsed_enodeb_id.txt
+egrep '1 ENodeBFunction|lget . eNBid' 4g_cell_status.txt > ${wdir}/4g_cell_status_parsed_enodeb_id_temp.txt
+perl -ne 'print "$1;" if /(\w+)>/; print "$1\n" if /^.+?ENodeBFunction.+?eNBId\s+(\w+).*$/' ${wdir}/4g_cell_status_parsed_enodeb_id_temp.txt | gsed -r -e 's/^.+?(RN_.+?)(;\w+)$/\1\2 /g' >> ${wdir}/4g_cell_status_parsed_enodeb_id.txt
 cp -p ${wdir}/4g_cell_status_parsed_enodeb_id.txt ${wdir}/data/4g_cell_status_parsed_enodeb_id_$(date '+DATE: %d_%m_%Y_%H_%M' | sed 's/DATE: //').txt
 
 #java -classpath /home/fmuser2/scripts/EricssonToOptima/ToOracle_ftpParserV7.jar ToOracle_fileLoad -path=/home/fmuser2/scripts/4g_status/4g_cell_status_parsed.txt -TblName=configuration.e_4g_status -TruncateTable
